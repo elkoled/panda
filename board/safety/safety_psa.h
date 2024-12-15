bc65acc128e27f910c0aff3b74a7c3ed5dd53581
elkoled <elkoled@gmail.com>
2024-12-15 18:35:51 +0100

    no errors

--- board/safety/safety_psa.h
+++ board/safety/safety_psa.h
@@ -14,125 +14,155 @@
 #define PSA_CAM_BUS  2U
 
 const CanMsg PSA_TX_MSGS[] = {
-  {PSA_LANE_KEEP_ASSIST, 0, 8},
+  // {PSA_LANE_KEEP_ASSIST, 0, 8},
 };
 
 RxCheck psa_rx_checks[] = {
   // TODO: counters and checksums
-  {.msg = {{PSA_DRIVER, PSA_CAM_BUS, 6, .frequency = 10U}, { 0 }, { 0 }}},
-  {.msg = {{PSA_DAT_BSI, PSA_CAM_BUS, 8, .frequency = 20U}, { 0 }, { 0 }}},
-  {.msg = {{PSA_HS2_DYN_ABR_38D, PSA_ADAS_BUS, 8, .frequency = 25U}, { 0 }, { 0 }}},
-  //{.msg = {{PSA_HS2_BGE_DYN5_CMM_228, PSA_ADAS_BUS, 8, .frequency = 100U}, { 0 }, { 0 }}},
-  {.msg = {{PSA_HS2_DAT_MDD_CMD_452, PSA_ADAS_BUS, 6, .frequency = 20U}, { 0 }, { 0 }}},
+  // {.msg = {{PSA_DRIVER, PSA_CAM_BUS, 6, .frequency = 10U}, { 0 }, { 0 }}},
+  // {.msg = {{PSA_DAT_BSI, PSA_CAM_BUS, 8, .frequency = 20U}, { 0 }, { 0 }}},
+  // {.msg = {{PSA_HS2_DYN_ABR_38D, PSA_ADAS_BUS, 8, .frequency = 25U}, { 0 }, { 0 }}},
+  // //{.msg = {{PSA_HS2_BGE_DYN5_CMM_228, PSA_ADAS_BUS, 8, .frequency = 100U}, { 0 }, { 0 }}},
+  // {.msg = {{PSA_HS2_DAT_MDD_CMD_452, PSA_ADAS_BUS, 6, .frequency = 20U}, { 0 }, { 0 }}},
 };
 
 static bool psa_lkas_msg_check(int addr) {
   return addr == PSA_LANE_KEEP_ASSIST;
 }
 
-// TODO: update rate limits
-const SteeringLimits PSA_STEERING_LIMITS = {
-  .angle_deg_to_can = 10,
-  .angle_rate_up_lookup = {
-    {0., 5., 15.},
-    {10., 1.6, .30},
-  },
-  .angle_rate_down_lookup = {
-    {0., 5., 15.},
-    {10., 7.0, .8},
-  },
-};
+// // TODO: update rate limits
+// const SteeringLimits PSA_STEERING_LIMITS = {
+//   .angle_deg_to_can = 10,
+//   .angle_rate_up_lookup = {
+//     {0., 5., 15.},
+//     {10., 1.6, .30},
+//   },
+//   .angle_rate_down_lookup = {
+//     {0., 5., 15.},
+//     {10., 7.0, .8},
+//   },
+// };
 
-static void psa_rx_hook(const CANPacket_t *to_push) {
+// static void psa_rx_hook(const CANPacket_t *to_push) {
+//   int bus = GET_BUS(to_push);
+//   int addr = GET_ADDR(to_push);
+
+//   if (bus == PSA_MAIN_BUS) {
+//     // Update brake pedal
+//     if (addr == PSA_DAT_BSI) {
+//       // Signal: P013_MainBrake
+//       brake_pressed = GET_BIT(to_push, 5);
+//     }
+
+//     bool stock_ecu_detected = psa_lkas_msg_check(addr);
+//     generic_rx_checks(stock_ecu_detected);
+//   }
+
+//   if (bus == PSA_ADAS_BUS) {
+//     // Update vehicle speed and in motion state
+//     if (addr == PSA_HS2_DYN_ABR_38D) {
+//       // Signal: VITESSE_VEHICULE_ROUES
+//       int speed = (GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1);
+//       vehicle_moving = speed > 0;
+//       UPDATE_VEHICLE_SPEED(speed * 0.01);
+//     }
+
+//     // Update gas pedal
+//     if (addr == PSA_DRIVER) {
+//       // Signal: GAS_PEDAL
+//       gas_pressed = GET_BYTE(to_push, 3) > 0U;
+//     }
+
+//     // Update cruise state
+//     if (addr == PSA_HS2_DAT_MDD_CMD_452) {
+//       // Signal: DDE_ACTIVATION_RVV_ACC
+//       pcm_cruise_check(GET_BIT(to_push, 23));
+//     }
+//   }
+// }
+
+// static bool psa_tx_hook(const CANPacket_t *to_send) {
+//   bool tx = true;
+//   int addr = GET_ADDR(to_send);
+
+//   // TODO: Safety check for cruise buttons
+//   // TODO: check resume is not pressed when controls not allowed
+//   // TODO: check cancel is not pressed when cruise isn't engaged
+
+//   // Safety check for LKA
+//   if (addr == PSA_LANE_KEEP_ASSIST) {
+//     // Signal: ANGLE
+//     int desired_angle = to_signed((GET_BYTE(to_send, 6) << 6) | ((GET_BYTE(to_send, 7) & 0xFCU) >> 2), 14);
+//     // Signal: STATUS
+//     bool lka_active = ((GET_BYTE(to_send, 4) & 0x18U) >> 3) == 2U;
+
+//     if (steer_angle_cmd_checks(desired_angle, lka_active, PSA_STEERING_LIMITS)) {
+//       tx = false;
+//     }
+//   }
+
+//   return tx;
+// }
+
+// static int psa_fwd_hook(int bus_num, int addr) {
+//   int bus_fwd = -1;
+//   switch (bus_num) {
+//     case PSA_MAIN_BUS: {
+//       if (psa_lkas_msg_check(addr)) {
+//         // Block stock LKAS messages
+//         bus_fwd = -1;
+//       } else {
+//         // Forward all other traffic from MAIN to CAM
+//         bus_fwd = PSA_CAM_BUS;
+//       }
+//       break;
+//     }
+//     case PSA_CAM_BUS: {
+//       // Forward all traffic from CAM to MAIN
+//       bus_fwd = PSA_MAIN_BUS;
+//       break;
+//     }
+//     default: {
+//       // No other buses should be in use; fallback to block
+//       bus_fwd = -1;
+//       break;
+//     }
+//   }
+//   return bus_fwd;
+// }
+void psa_rx_hook(const CANPacket_t *to_push) {
   int bus = GET_BUS(to_push);
   int addr = GET_ADDR(to_push);
-
-  if (bus == PSA_MAIN_BUS) {
-    // Update brake pedal
-    if (addr == PSA_DAT_BSI) {
-      // Signal: P013_MainBrake
-      brake_pressed = GET_BIT(to_push, 5);
-    }
-
-    bool stock_ecu_detected = psa_lkas_msg_check(addr);
-    generic_rx_checks(stock_ecu_detected);
+  bool stock_ecu_detected = psa_lkas_msg_check(addr);
+  if((bus==2)&& stock_ecu_detected)
+  {
+    print("STOCK_ECU!\n");
   }
-
-  if (bus == PSA_ADAS_BUS) {
-    // Update vehicle speed and in motion state
-    if (addr == PSA_HS2_DYN_ABR_38D) {
-      // Signal: VITESSE_VEHICULE_ROUES
-      int speed = (GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1);
-      vehicle_moving = speed > 0;
-      UPDATE_VEHICLE_SPEED(speed * 0.01);
-    }
-
-    // Update gas pedal
-    if (addr == PSA_DRIVER) {
-      // Signal: GAS_PEDAL
-      gas_pressed = GET_BYTE(to_push, 3) > 0U;
-    }
-
-    // Update cruise state
-    if (addr == PSA_HS2_DAT_MDD_CMD_452) {
-      // Signal: DDE_ACTIVATION_RVV_ACC
-      pcm_cruise_check(GET_BIT(to_push, 23));
-    }
-  }
+  generic_rx_checks((bus==2) && stock_ecu_detected);
 }
 
 static bool psa_tx_hook(const CANPacket_t *to_send) {
-  bool tx = true;
-  int addr = GET_ADDR(to_send);
-
-  // TODO: Safety check for cruise buttons
-  // TODO: check resume is not pressed when controls not allowed
-  // TODO: check cancel is not pressed when cruise isn't engaged
-
-  // Safety check for LKA
-  if (addr == PSA_LANE_KEEP_ASSIST) {
-    // Signal: ANGLE
-    int desired_angle = to_signed((GET_BYTE(to_send, 6) << 6) | ((GET_BYTE(to_send, 7) & 0xFCU) >> 2), 14);
-    // Signal: STATUS
-    bool lka_active = ((GET_BYTE(to_send, 4) & 0x18U) >> 3) == 2U;
-
-    if (steer_angle_cmd_checks(desired_angle, lka_active, PSA_STEERING_LIMITS)) {
-      tx = false;
-    }
-  }
-
-  return tx;
+  print("psa_tx_hook\n");
+  UNUSED(to_send);
+  return true;
 }
 
 static int psa_fwd_hook(int bus_num, int addr) {
   int bus_fwd = -1;
-  switch (bus_num) {
-    case PSA_MAIN_BUS: {
-      if (psa_lkas_msg_check(addr)) {
-        // Block stock LKAS messages
-        bus_fwd = -1;
-      } else {
-        // Forward all other traffic from MAIN to CAM
-        bus_fwd = PSA_CAM_BUS;
-      }
-      break;
-    }
-    case PSA_CAM_BUS: {
-      // Forward all traffic from CAM to MAIN
-      bus_fwd = PSA_MAIN_BUS;
-      break;
-    }
-    default: {
-      // No other buses should be in use; fallback to block
-      bus_fwd = -1;
-      break;
-    }
+  UNUSED(addr);
+  print("psa_fwd_hook\n");
+  if (bus_num == 0) {
+    bus_fwd = 2;
   }
+  if (bus_num == 2) {
+    bus_fwd = 0;
+  }
+
   return bus_fwd;
 }
-
 static safety_config psa_init(uint16_t param) {
   UNUSED(param);
+  print("psa_init\n");
   return BUILD_SAFETY_CFG(psa_rx_checks, PSA_TX_MSGS);
 }
 
