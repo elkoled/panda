f697df8cb930b358698179c04a1a89365eecdfc3
elkoled <elkoled@gmail.com>
2024-12-21 15:18:40 +0100

    increase tx limits

--- board/safety/safety_psa.h
+++ board/safety/safety_psa.h
@@ -31,21 +31,22 @@ static bool psa_lkas_msg_check(int addr) {
 }
 
 // TODO: update rate limits, copied from toyota
+// TODO: multiplied by 100 to avoid checks failed
 const SteeringLimits PSA_STEERING_LIMITS = {
-    .max_steer = 100,
-    .max_rate_up = 15,          // ramp up slow
-    .max_rate_down = 25,        // ramp down fast
-    .max_torque_error = 100,    // max torque cmd in excess of motor torque
-    .max_rt_delta = 450,        // the real time limit is 1800/sec, a 20% buffer
-    .max_rt_interval = 250000,
+    .max_steer = 10000,
+    .max_rate_up = 1500,          // ramp up slow
+    .max_rate_down = 2500,        // ramp down fast
+    .max_torque_error = 10000,    // max torque cmd in excess of motor torque
+    .max_rt_delta = 45000,        // the real time limit is 1800/sec, a 20% buffer
+    .max_rt_interval = 25000000,
     .type = TorqueMotorLimited,
 
     // the EPS faults when the steering angle rate is above a certain threshold for too long. to prevent this,
     // we allow setting STEER_REQUEST bit to 0 while maintaining the requested torque value for a single frame
-    .min_valid_request_frames = 18,
-    .max_invalid_request_frames = 1,
-    .min_valid_request_rt_interval = 170000,  // 170ms; a ~10% buffer on cutting every 19 frames
-    .has_steer_req_tolerance = true,
+    // .min_valid_request_frames = 18,
+    // .max_invalid_request_frames = 1,
+    // .min_valid_request_rt_interval = 170000,  // 170ms; a ~10% buffer on cutting every 19 frames
+    // .has_steer_req_tolerance = true,
 
     // LTA angle limits
     // factor for STEER_TORQUE_SENSOR->STEER_ANGLE and STEERING_LTA->STEER_ANGLE_CMD (1 / 0.0573)
@@ -98,25 +99,30 @@ static void psa_rx_hook(const CANPacket_t *to_push) {
 
 static bool psa_tx_hook(const CANPacket_t *to_send) {
   bool tx = true;
-  UNUSED(to_send);
+  // UNUSED(to_send);
   // TODO: enable tx safeety checks
-  // int addr = GET_ADDR(to_send);
+  int addr = GET_ADDR(to_send);
 
   // TODO: Safety check for cruise buttons
   // TODO: check resume is not pressed when controls not allowed
   // TODO: check cancel is not pressed when cruise isn't engaged
 
-  // // Safety check for LKA
-  // if (addr == PSA_LANE_KEEP_ASSIST) {
-  //   // Signal: TORQUE
-  //   int desired_torque = to_signed((GET_BYTES(to_send, 3, 4) & 0xFFE0) >> 5, 11);
-  //   // Signal: STATUS
-  //   bool lka_active = ((GET_BYTE(to_send, 4) & 0x18U) >> 3) == 2U;
+  // Safety check for LKA
+  if (addr == PSA_LANE_KEEP_ASSIST) {
+    // Signal: TORQUE
+    int desired_torque = to_signed((GET_BYTES(to_send, 3, 4) & 0xFFE0) >> 5, 11);
+    // Signal: STATUS
+    bool lka_active = ((GET_BYTE(to_send, 4) & 0x18U) >> 3) == 2U;
+    print("desired_torque: ");
+    puth(desired_torque);
+    print(" lka_active: ");
+    puth(lka_active);
+    print("\n");
 
-  //   if (steer_torque_cmd_checks(desired_torque, lka_active, PSA_STEERING_LIMITS)) {
-  //      tx = false;
-  //   }
-  // }
+    if (steer_torque_cmd_checks(desired_torque, lka_active, PSA_STEERING_LIMITS)) {
+       tx = false;
+    }
+  }
 
   return tx;
 }
