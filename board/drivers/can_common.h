8dc793be0ae2630ee77d23b9356286782bb4361f
elkoled <elkoled@gmail.com>
2025-12-19 19:14:12 +0100

    move to bus2

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -201,6 +201,9 @@ void ignition_can_hook(CANPacket_t *msg) {
       ignition_can_cnt = 0U;
     }
 
+  } else if(msg->bus == 2) {
+
+    int len = GET_LEN(msg);
     // PSA exception
     if ((msg->addr == 0x432) && (len == 8)) {
       int counter = msg->data[1] & 0xFU;
