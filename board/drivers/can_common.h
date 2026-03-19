a2c367c64574b20993d52f75af15994fa17fc208
elkoled <elkoled@gmail.com>
2026-03-19 08:31:22 -0700

    PSA ignition

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -201,6 +201,13 @@ void ignition_can_hook(CANPacket_t *msg) {
       ignition_can_cnt = 0U;
     }
 
+    // PSA exception
+    if ((msg->addr == 0x348U) && (len == 8)) {
+      // bit 41: EV running, bit 42: ICE running
+      ignition_can = (msg->data[5] & 0x6U) != 0U;
+      ignition_can_cnt = 0U;
+    }
+
   }
 }
 
