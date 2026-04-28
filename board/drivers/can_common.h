65dc00dc60b9b316f9542aab232c142687545c01
elkoled <elkoled@gmail.com>
2026-04-27 23:59:11 -0700

    Volkswagen MEB: ignition exception

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -201,6 +201,12 @@ void ignition_can_hook(CANPacket_t *msg) {
       ignition_can_cnt = 0U;
     }
 
+    // Volkswagen MEB exception
+    if ((msg->addr == 0x3C0U) && (len == 4)) {
+      ignition_can = (msg->data[2] >> 1) & 1U;
+      ignition_can_cnt = 0U;
+    }
+
   }
 }
 
